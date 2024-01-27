911bc9356d4c1fc1fe0148923651f08f273515f6
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2024-01-27 22:04:10 +0700

    consume provider in child

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,23 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:prov_app/providers/products.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  // final String title;
-  // final double price;
-
-  // ProductDetailScreen(this.title, this.price);
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
     final productId =
         ModalRoute.of(context)!.settings.arguments as String; // is the id!
-    // ...
+    final product = Provider.of<Products>(context)
+        .allproducts
+        .firstWhere((prod) => prod.id == productId);
     return Scaffold(
       appBar: AppBar(
         title: Text('Product Details'),
       ),
-      body: Center(
-        child: Text("INI ADALAG PAGE PRODUK ($productId)"),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 250,
+            child: Image.network(
+              "${product.imageUrl}",
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(height: 30),
+          Text(
+            "${product.title}",
+            style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 15),
+          Text(
+            "\$${product.price}",
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+          SizedBox(height: 15),
+          Text(
+            "${product.description}",
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+        ],
       ),
     );
   }
