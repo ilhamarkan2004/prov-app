5c3be4c22987c7f33c0e97671ded581833784369
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2024-01-27 21:35:43 +0700

    add providers

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:prov_app/models/product.dart';
 
-class Products with ChangeNotifier{
-  
+class Products with ChangeNotifier {
+  List<Product> _allproducts = [];
+
+  List<Product> get allproducts {
+    return [..._allproducts];
+  }
+
+  void addproduct(value) {
+    _allproducts.add(value);
+    notifyListeners();
+  }
 }
